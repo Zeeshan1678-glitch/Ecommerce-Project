8b1335e59afaa5a8b6d5a6024d4eb31ea28130c5
Zeeshan Ahmed <zazeeshan@1678gmail.com>
2025-09-02 03:06:58 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Routes, Route } from 'react-router';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
+import './App.css'
+
+function App() {
+  
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route> 
+      <Route path="checkout" element={<CheckoutPage />}></Route>
+    </Routes>
+  )
+}
+
+export default App;
