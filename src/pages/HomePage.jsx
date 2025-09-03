8ae4aebc6ac9076c3de3b41d8cef71c5f6111ae3
Zeeshan Ahmed <zazeeshan@1678gmail.com>
2025-09-03 06:14:20 -0700

    use data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
 import axios from 'axios'
+import { useEffect ,useState} from 'react'
 import './header.css'
-import {products} from '../../starting-code/data/products.js'
 import './HomePage.css'
 
+
+
+
 export function HomePage() {
+    const [products, setProducts] = useState([]);
+
+   useEffect(() => {
 
     axios.get('http://localhost:3000/api/products')
-        .then(response => {
-             response.data;
+        .then((response) => {
+            setProducts(response.data);
         })
-        
+   }, []);
+
 
   return (
     <>
